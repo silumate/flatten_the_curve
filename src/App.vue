a1a65c05f869be0aa91b7ea674b9c37450721025
kevinator <63034792+silumate@users.noreply.github.com>
2020-07-31 00:14:55 -0700

    implement bar chart rendering in a vue component.

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,75 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!---
     <BarChart />
+    <LineChart />
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    --->
+    <form action="#">
+      <div class="form-group">
+<!--
+        <input
+          type="text"
+          placeholder="owner/repo Name"
+          v-model="repository"
+          class="col-md-2 col-md-offset-5"
+        />
+-->
+        <button v-on:click='runSimulation'>Run Simulation</button>
+      </div>
+    </form>
+    <BarTimeSeries :series="series" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import BarChart from './components/BarChart.vue'
+/*
+import LineChart from "./components/LineChart.vue";
+import BarChart from "./components/BarChart.vue";
+import HelloWorld from "./components/HelloWorld.vue";
+*/
+import BarTimeSeries from "./components/BarTimeSeries.vue"
+
+Date.prototype.formatMMDD = function(){
+    return (this.getMonth() + 1) + 
+    "/" +  this.getDate()
+}
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
+    BarTimeSeries
+    /*
     HelloWorld,
-    BarChart
+    BarChart,
+    LineChart
+    */
+  },
+  data() {
+    return {
+      series: []
+    }
+  },
+  methods: {
+    runSimulation() {
+      const startDate = new Date("2020-01-01T00:00:00")
+      const endDate = new Date("2021-01-01T00:00:00")
+
+      let peep_infected = []
+      let x = 0
+      for (;;) {
+        const newDay = new Date(startDate.valueOf())
+        newDay.setDate(newDay.getDate() + x)
+        if (newDay >= endDate) break
+        const sick = 340000 - ((x - 183)**2)
+        peep_infected.push( {day: newDay, infected: sick} )
+        x++
+        if (x > 367) break
+      }
+      this.series = peep_infected
+    }
   }
-}
+};
 </script>
 
 <style>
@@ -27,5 +80,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.bar {
+  fill: #319bbe;
 }
 </style>
---
+++ src/components/BarTimeSeries.vue
@@ -0,0 +1,128 @@
+<template>
+  <div>
+    <svg />
+  </div>
+</template>
+
+<script>
+import * as d3 from "d3";
+import _ from "lodash";
+
+export default {
+  props: ["series"],
+  data() {
+    return {
+      chart: null
+    };
+  },
+  watch: {
+    series(val) {
+      if (this.chart != null) this.chart.remove();
+      this.renderChart(val);
+    }
+  },
+  methods: {
+    renderChart(series_val) {
+      const margin = 100;
+      const svg_width = 1000;
+      const svg_height = 600;
+      const chart_width = svg_width - margin;
+      const chart_height = svg_height - 2 * margin;
+      const barWidth = (chart_width / series_val.length) - 0.2
+
+      const svg = d3
+        .select("svg")
+        .attr("width", svg_width)
+        .attr("height", svg_height)
+
+      this.chart = svg
+        .append("g")
+        .attr("transform", `translate(${margin}, ${margin})`)
+
+      const maxY = _.maxBy(series_val, "infected").infected
+
+      const yScale = d3
+        .scaleLinear()
+        .range([chart_height, 0])
+        .domain([0, maxY])
+
+      this.chart.append("g").call(d3.axisLeft(yScale).ticks(5));
+
+      const xScale = d3
+        .scaleTime()
+        .range([0, chart_width])
+        .domain([series_val[0].day, series_val[series_val.length -1].day])
+
+      const xAxis = d3
+        .axisBottom(xScale)
+        .tickFormat(d3.timeFormat("%B"))
+        // .tickFormat(d3.timeFormat("%m/%d"))
+        .ticks(12)
+
+      this.chart
+        .append("g")
+        .attr("transform", `translate(0, ${chart_height})`)
+        .call(xAxis)
+        .selectAll("text")
+        .style("text-anchor", "start")
+
+      const barGroups = this.chart
+        .selectAll("rect")
+        .data(series_val)
+        .enter()
+
+      barGroups
+        .append("rect")
+        .attr("class", "bar")
+        .attr("x", o => xScale(o.day))
+        .attr("y", o => yScale(o.infected))
+        .attr("height", o => chart_height - yScale(o.infected))
+        .attr("width", barWidth)
+        .on("mouseenter", function(actual, i) {
+          d3.select(this)
+            .transition()
+            .duration(100)
+            .attr("opacity", 0.6)
+          barGroups
+            .append("text")
+            .attr("class", "value")
+            .attr("x", o => xScale(o.day) + barWidth / 2)
+            .attr("y", o => yScale(o.infected) - 20)
+            .attr("text-anchor", "middle")
+            .text((o, idx) => {
+              return idx !== i ? "" : `${o.day.formatMMDD()} - ${o.infected} infected`;
+            })
+        })
+        .on("mouseleave", function() {
+          d3.selectAll(".infected").attr("opacity", 1)
+
+          d3.select(this)
+            .transition()
+            .duration(300)
+            .attr("opacity", 1)
+            .attr("x", o => xScale(o.day))
+            .attr("width", barWidth)
+
+          svg.selectAll(".value").remove()
+        })
+
+      svg
+        .append("text")
+        .attr("class", "label")
+        .attr("x", -(chart_height / 2) - margin)
+        .attr("y", 12)
+        .attr("transform", "rotate(-90)")
+        .attr("text-anchor", "middle")
+        .text("Infected")
+
+      svg
+        .append("text")
+        .attr("class", "title")
+        .attr("x", chart_width / 2 + margin)
+        .attr("y", 40)
+        .attr("text-anchor", "middle")
+        .text("All who are and were infected")
+    }
+  }
+};
+</script>
